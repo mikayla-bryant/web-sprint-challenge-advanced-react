cd92d5157d2b0e5675a0a39b0c91000a81bd8248
mikayla-bryant <mikaylabryant25@gmail.com>
2021-01-15 12:27:30 -0500

    Completed sprint challenge

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+
+import React, { useState } from 'react';
+const useForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues);
+  const handleChanges = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+  return [values, handleChanges];
+};
+
+export default useForm;
